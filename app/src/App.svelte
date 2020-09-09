5dde7e40fc2dd1489ac81675b00b7e4607f8190c
bcavs <cavenagh.ben@gmail.com>
2020-09-08 21:22:36 -0500

    add RxJS and start the ajax call to get a user

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-	export let name;
+  import { ajax } from "rxjs/ajax";
+  import { pluck, startWith } from "rxjs/operators";
+
+  export let name;
+
+  const apiURL = "https://api.github.com/search/repositories?q=rxjs";
+
+  const reposObservable = ajax(apiURL).pipe(
+    pluck("response"),
+    pluck("items"),
+    startWith([])
+  );
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  <h1>Hello {name}!</h1>
+  <input type="text" bind:value={name} />
+</main>
